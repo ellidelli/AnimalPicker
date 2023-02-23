29259c68cd156a7161b2a8dea578155803292150
ellidelli <elizabeth.delea@gmail.com>
2023-02-23 12:52:22 -0500

    ASCII art

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -3,14 +3,15 @@ public class AnimalPicker {
     public static void main (String args[]) {
         Scanner scan = new Scanner(System.in);
         boolean valid = false;
+        
         while (valid == false) {
             System.out.println("\nWould you like to see a cat or a dog?");
             String choice = scan.nextLine();
             if (choice.equalsIgnoreCase ("cat")) {
-                System.out.println("ASCII cat");
+                System.out.println( "/\\__/\\\n( o.o )\n > ^ <");
                 valid = true;
             } else if (choice.equalsIgnoreCase("dog")) {
-                System.out.println("ASCII dog");
+                System.out.println("  __      _\no'')}____//\n`_/      )\n(_(_/-(_/");
                 valid = true;
             } else {
                 System.out.print("Invalid response\n");
